847d78277804a97735b2ff8297562ade2a12cb74
Björn Hvass <bjohv276@li17-23.ida.liu.se>
2016-09-08 09:39:19 +0200

    Added the files for lab 2

---
+++ lab2/evilhangman/src/evilhangman.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+
+int main() {
+    cout << "Welcome to Hangman." << endl;
+
+    // TODO: Finish the program!
+
+    return 0;
+}
---
+++ lab2/wordchain/src/wordchain.cpp
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+
+int main() {
+    cout << "Welcome to TDDD86 Word Chain." << endl;
+    cout << "If you give me two English words, I will transform the" << endl;
+    cout << "first into the second by changing one letter at a time." << endl;
+    cout << endl;
+
+    cout << "Please type two words: ";
+
+    // TODO: Finish the program!
+
+    return 0;
+}
