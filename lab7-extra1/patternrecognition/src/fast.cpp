d73c8adcaae89601b0ac54ca3018c7ea74662d4d
Björn Hvass <bjohv276@li17-5.ida.liu.se>
2016-12-13 18:08:08 +0100

    Finished extra-lab8

--- lab7-extra1/patternrecognition/src/fast.cpp
+++ lab7-extra1/patternrecognition/src/fast.cpp
@@ -13,37 +13,59 @@
 static const int SCENE_WIDTH = 512;
 static const int SCENE_HEIGHT = 512;
 
-void render_points(QGraphicsScene* scene, const vector<Point>& points) {
+int i = 0; //linecount
+
+void render_points(QGraphicsScene* scene, const vector<Point*>& points) {
     for(const auto& point : points) {
-        point.draw(scene);
+        point->draw(scene);
     }
 }
 
 void render_line(QGraphicsScene* scene, const Point& p1, const Point& p2) {
     p1.lineTo(scene, p2);
+    i++;
 }
 
-bool isNewLine(set<Point>* newPoints, map<Point*, vector<Point*>*>* lineMap){
+/*
+ * Checks if the given set describes a new line and not one previously found.
+ * This is done by searching in the map of points.
+ */
+bool isNewLine(set<Point*, bool(*)(Point* p1, Point* p2)>* newPoints, map<Point*, vector<Point*>*>* lineMap){
+
     vector<Point*>* lineVector;
-    lineMap->find(newPoints->begin())->second;
+    Point* newFirstPoint = *newPoints->begin();
 
-    for(Point* p: newPoints){
-        if(lineMap)
+    auto mapElemFound = lineMap->find(newFirstPoint);
+
+    if(mapElemFound == lineMap->end()){
+        return true;
     }
+
+    lineVector = mapElemFound->second;
+    Point* secondPoint = *(++newPoints->begin());
+
+    return find(lineVector->begin(), lineVector->end(), secondPoint) == lineVector->end();
 }
 
-void finishSet(const Point& curP, set<Point>* points, QGraphicsScene* scene, map<Point*, vector<Point*>*>* lineMap){
-    if(points->size() >= 3){
-        points->insert(curP);
-        render_line(scene, *points->begin(), *points->rbegin());
+/*
+ * Controls if given set should be drawn as a new line.
+ * If set is accepted a new line is drawn and the data about the new line is added to the map of lines.
+ */
+void finishSet(Point& curP, set<Point*, bool(*)(Point* p1, Point* p2)>* points, QGraphicsScene* scene, map<Point*, vector<Point*>*>* lineMap){
+    if(points->size() >= 3 && isNewLine(points, lineMap)){
+        points->insert(&curP);
+        render_line(scene, **points->begin(), **points->rbegin());
 
-        vector<Point*>* newVector = new vector<Point*>();
-        for(Point p : *points){
-            newVector->push_back(&p);
-        }
 
-        for(Point p : *points){
-            auto insert = lineMap->insert(pair<Point*, vector<Point*>*>(&p,newVector));
+
+        for(Point* p : *points){
+
+            vector<Point*>* newVector = new vector<Point*>();
+            for(Point* p : *points){
+                newVector->push_back(p);
+            }
+
+            auto insert = lineMap->insert(pair<Point*, vector<Point*>*>(p,newVector));
             if(!insert.second){
                 vector<Point*>* oldVector = insert.first->second;
 
@@ -64,7 +86,7 @@ int main(int argc, char *argv[]) {
     input.open(filename);
 
     // the vector of points
-    vector<Point> points;
+    vector<Point*> points;
 
     // read points from file
     int N;
@@ -75,7 +97,7 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; i < N; ++i) {
         input >> x >> y;
-        points.push_back(Point(x, y));
+        points.push_back(new Point(x, y));
     }
     input.close();
 
@@ -98,16 +120,18 @@ int main(int argc, char *argv[]) {
     map<Point*, vector<Point*>*> drawnPoints;
 
     for (int i = 0 ; points.size() > 2; i++) {
-        Point& curP = points.at(points.size()-1);
+        Point curP = *points.at(points.size()-1);
         points.pop_back();
 
-        auto dSlope = [&curP](const Point& p1, const Point& p2) -> bool{return p1.slopeTo(curP) < p2.slopeTo(curP);};
+        auto dSlope = [&curP](const Point* p1, const Point* p2) -> bool{return p1->slopeTo(curP) > p2->slopeTo(curP);};
         sort(points.begin(), points.end(), dSlope);
 
-        set<Point> curTilt = {points.at(0)};
+        auto pointerCompare = [](Point* p1, Point* p2){return *p1 < *p2;};
+        set<Point*, bool(*)(Point* p1, Point* p2)> curTilt(pointerCompare);
+        curTilt.insert(points.at(0));
 
         for(int j = 1; j < points.size(); j++){
-            if(points.at(j).slopeTo(curP) == curTilt.begin()->slopeTo(curP)){
+            if(points.at(j)->slopeTo(curP) == (*curTilt.begin())->slopeTo(curP)){
                 curTilt.insert(points.at(j));
             }
             else{
@@ -121,8 +145,16 @@ int main(int argc, char *argv[]) {
         finishSet(curP, &curTilt, scene, &drawnPoints);
     }
 
+    //Fix memory leaks
+    for(auto e : drawnPoints){
+        delete e.second;
+    }
+    for(Point* p : points){
+        delete p;
+    }
+
     auto end = chrono::high_resolution_clock::now();
-    cout << "Computing line segments took "
+    cout << "Computing " << i << " line segments took "
          << std::chrono::duration_cast<chrono::milliseconds>(end - begin).count()
          << " milliseconds." << endl;
 
